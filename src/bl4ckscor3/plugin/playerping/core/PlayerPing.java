908a660956c932aeaaa585035db12f1b30da95ae
bl4ckscor3 <donalddani9612@gmx.de>
2015-01-30 20:38:25 +0100

    added config reloading command

--- src/bl4ckscor3/plugin/playerping/core/PlayerPing.java
+++ src/bl4ckscor3/plugin/playerping/core/PlayerPing.java
@@ -1,5 +1,9 @@
 package bl4ckscor3.plugin.playerping.core;
 
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import bl4ckscor3.plugin.playerping.listener.ChatListener;
@@ -13,10 +17,30 @@ public class PlayerPing extends JavaPlugin
 		Config.createConfig(this);
 		System.out.println("[PlayerPing] v" + getDescription().getVersion() + " enabled!");
 	}
-	
+
 	@Override
 	public void onDisable()
 	{
 		System.out.println("[PlayerPing] v" + getDescription().getVersion() + " disabled!");
+	}
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
+	{
+		Player p = null;
+
+		if(sender instanceof Player)
+			p = (Player)sender;
+
+		if(cmd.getName().equalsIgnoreCase("playerpingreload"))
+		{
+			if(p.hasPermission("playerping.reload"))
+			{
+				reloadConfig();
+				p.sendMessage("[" + ChatColor.BLUE + "PlayerPing" + ChatColor.RESET + "] Reloaded configuration successfully.");
+				return true;
+			}
+		}
+		return false;
 	}
 }
