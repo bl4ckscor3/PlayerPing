0bc0c35209f61f50883881fac3802046289211be
bl4ckscor3 <donalddani9612@gmx.de>
2015-02-03 13:19:04 +0100

    config fix

--- src/bl4ckscor3/plugin/playerping/core/Config.java
+++ src/bl4ckscor3/plugin/playerping/core/Config.java
@@ -7,7 +7,7 @@ public class Config
 	public static void createConfig(Plugin plugin)
 	{
 		plugin.reloadConfig();
-		plugin.getConfig().options().header("Add the symbol(s) you want to prepend to a player's name under name.prefix, and the ones you like to append under name.suffix. Both are strings.\nE.g.: The vanilla chat format is \"<playerName>\". In this case, name.prefix would default to '<' and name.suffix would default to '>'.\nA space between the suffix and the message is automatically added.\nIf you don't want that to happen, setDefault \"space\" to \"false\"");
+		plugin.getConfig().options().header("Add the symbol(s) you want to prepend to a player's name under name.prefix, and the ones you like to append under name.suffix. Both are strings.\nE.g.: The vanilla chat format is \"<playerName>\". In this case, name.prefix would default to '<' and name.suffix would default to '>'.\nA space between the suffix and the message is automatically added.\nIf you don't want that to happen, set \"space\" to \"false\"");
 		plugin.getConfig().addDefault("name.prefix", "<");
 		plugin.getConfig().addDefault("name.suffix", ">");
 		plugin.getConfig().addDefault("space", true);
